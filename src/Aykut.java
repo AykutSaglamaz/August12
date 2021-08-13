0c2f7b6fbb567eb967493da82a8d9017f91c3ec3
AykutSaglamaz <88285272+AykutSaglamaz@users.noreply.github.com>
2021-08-12 22:35:01 -0400

    Update Aykut.java

--- src/Aykut.java
+++ src/Aykut.java
@@ -1,6 +1,8 @@
 public class Aykut {
     public static void main(String[] args) {
         System.out.println("Aykut");
+        System.out.println("Aykut");
+        System.out.println("Aykut");
 
 
 
